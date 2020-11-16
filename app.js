7ee29623067dc3538de7fa0564c4e77ace15f563
Kenji <kenji.arakaki93@gmail.com>
2020-11-16 14:14:48 -0300

    sweet alert

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ function pronounce() {
 function reset() {
     document.getElementById('input-text').value = "";
 }
+
+//sweet alert
+function about() {
+    Swal.fire({
+        title: '<strong>About</strong>',
+        icon: 'info',
+        html:
+          'Escribí, pegá textos o palabras en inglés y conocé su pronunciación</br> ' +
+          '<strong>Cambiá los números por letras:</strong> <br>' +
+          'Ex: 16 = sixteen',
+        showCloseButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> OK!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+      })
+}
