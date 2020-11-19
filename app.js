fa2f15421778991217a46c60a9d9c60e7fcdfbfb
Kenji <kenji.arakaki93@gmail.com>
2020-11-19 11:29:27 -0300

    Ofuscando js

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-let txt = new SpeechSynthesisUtterance();
-let voices = speechSynthesis.getVoices();
-txt.voice = voices[6];
-txt.pitch = 1;
-txt.lang = 'en-US';
-
-
-function pronounce() {   
-    let it = document.getElementById('input-text').value;
-    if(it.length =! '') {
-        txt.text = it;
-        speechSynthesis.speak(txt);
-    }
-}
-
-function reset() {
-    document.getElementById('input-text').value = "";
-}
+const _0x55c4=['input-text','voice','lang','text','speak','pitch','value','en-US','getElementById'];(function(_0x1f0584,_0x10969e){const _0x55c43b=function(_0x22fadd){while(--_0x22fadd){_0x1f0584['push'](_0x1f0584['shift']());}};_0x55c43b(++_0x10969e);}(_0x55c4,0x155));const _0x22fa=function(_0x1f0584,_0x10969e){_0x1f0584=_0x1f0584-0x1cc;let _0x55c43b=_0x55c4[_0x1f0584];return _0x55c43b;};const _0xed9a4b=_0x22fa;let txt=new SpeechSynthesisUtterance(),voices=speechSynthesis['getVoices']();txt[_0xed9a4b(0x1ce)]=voices[0x6],txt[_0xed9a4b(0x1d2)]=0x1,txt[_0xed9a4b(0x1cf)]=_0xed9a4b(0x1d4);function pronounce(){const _0x40b480=_0xed9a4b;let _0x5e0392=document['getElementById'](_0x40b480(0x1cd))[_0x40b480(0x1d3)];(_0x5e0392['length']=!'')&&(txt[_0x40b480(0x1d0)]=_0x5e0392,speechSynthesis[_0x40b480(0x1d1)](txt));}function reset(){const _0x517f72=_0xed9a4b;document[_0x517f72(0x1cc)](_0x517f72(0x1cd))[_0x517f72(0x1d3)]='';}
 
 //sweet alert
 function about() {
