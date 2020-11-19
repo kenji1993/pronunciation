6087818bcb7ad1827ba8f026e6fc77efc6b6f75f
Kenji <kenji.arakaki93@gmail.com>
2020-11-19 12:52:54 -0300

    Debuggeando eje

--- js/eje.js
+++ js/eje.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded",function() {
-    let content = document.getElementById('content');
+    let main = document.getElementById("main");
 
     window.addEventListener('deviceorientation', function(e) {
         a = Math.floor(e.alpha);
         b = Math.floor(e.beta);
-        c = Math.floor(e.gamma);
+        g = Math.floor(e.gamma);
         
-        let transform = 'rotateZ('+ Math.round(e.alpha) +' deg) rotateX('+ Math.round(e.beta -90) +' deg) rotateY('+ Math.round(e.gamma) +' deg)';
-
-        content.style.transform = transform;
+        let transform = 'rotateZ(' + Math.round(e.alpha) + 'deg) rotateX(' + Math.round(e.beta -90) + 'deg) rotateY(' + Math.round(e.gamma) + 'deg)';
+        main.style.transform = transform;
     });
 })
